c3d37c35fcb5e706b8cc17a10a761012f69eaeb9
Heesu Lee <hesue615@gmail.com>
2020-10-21 23:32:13 +0900

    initial commit

---
+++ api-front/src/main/java/kr/creedit/CreeditWebApplication.java
@@ -0,0 +1,12 @@
+package kr.creedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreeditWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreeditWebApplication.class, args);
+    }
+}
