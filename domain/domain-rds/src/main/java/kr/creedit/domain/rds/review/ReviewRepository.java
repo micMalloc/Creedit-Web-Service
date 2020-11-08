9dc0fde6ff3f8fc9950a9e283c71c2ed21e691b1
Heesu Lee <hesue615@gmail.com>
2020-11-08 16:07:38 +0900

    [CRDT-4] Review 도메인 생성

---
+++ domain/domain-rds/src/main/java/kr/creedit/domain/rds/review/ReviewRepository.java
@@ -0,0 +1,10 @@
+package kr.creedit.domain.rds.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByCategoryId(Long categoryId);
+}
---
+++ domain/domain-rds/src/test/java/kr/creedit/domain/rds/DomainRdsApplicationTests.java
@@ -0,0 +1,14 @@
+package kr.creedit.domain.rds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class DomainRdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
