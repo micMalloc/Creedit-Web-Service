27347ba7cf09d9834a59e65ebc272da0fcf54ace
이희수 <heesu_lee@wemakeprice.com>
2020-11-29 17:19:54 +0900

    [CRDT-7] YouTubeClient 예외 처리 및 실패 테스트 추가, 불필요한 의존성 제거

--- creedit-clients/client-youtube/src/main/java/kr/creedit/client/youtube/dto/ChannelStatisticsDto.java
+++ creedit-clients/client-youtube/src/main/java/kr/creedit/client/youtube/dto/ChannelStatisticsDto.java
@@ -1,11 +1,13 @@
 package kr.creedit.client.youtube.dto;
 
+import kr.creedit.domain.rds.youtube.statistics.Statistics;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -15,20 +17,41 @@ public class ChannelStatisticsDto {
 
     private List<Item> items;
 
+    public Statistics getStatistics() {
+        return Optional.ofNullable(items)
+                .map(item -> item.get(0))
+                .map(Item::toEntity)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
     @Getter
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
     private static class Item {
         private StatisticsDto statistics;
+
+        private Statistics toEntity() {
+            return statistics.toEntity();
+        }
     }
 
     @Getter
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
     private static class StatisticsDto {
+
         private Long viewCount;
         private Long subscriberCount;
         private boolean hiddenSubscriberCount;
         private Long videoCount;
+
+        private Statistics toEntity() {
+            return Statistics.builder()
+                    .viewCount(viewCount)
+                    .videoCount(videoCount)
+                    .hiddenSubscriberCount(hiddenSubscriberCount)
+                    .subscriberCount(subscriberCount)
+                    .build();
+        }
     }
 }
--- creedit-clients/client-youtube/src/test/java/kr/creedit/client/youtube/YouTubeClientTest.java
+++ creedit-clients/client-youtube/src/test/java/kr/creedit/client/youtube/YouTubeClientTest.java
@@ -1,10 +1,15 @@
 package kr.creedit.client.youtube;
 
 import kr.creedit.client.youtube.dto.ChannelStatisticsDto;
+import kr.creedit.domain.rds.youtube.statistics.Statistics;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ActiveProfiles({"client-youtube", "test"})
 @SpringBootTest
@@ -13,6 +18,7 @@ class YouTubeClientTest {
     @Autowired
     private YouTubeClient youTubeClient;
 
+    @DisplayName("채널 통계 정보 획득 테스트")
     @Test
     void getStatisticsForChannel_test() {
         // given
@@ -23,6 +29,23 @@ class YouTubeClientTest {
                 .block();
 
         // then
-        System.out.println(response);
+        assert response != null;
+        assertThat(response.getStatistics()).isInstanceOf(Statistics.class);
+    }
+
+    @DisplayName("존재하지 않는 채널 통계 획득 요청 테스트")
+    @Test
+    void getStatisticsForChannel_fail_test() {
+        // given
+        String channelId = "no channel";
+
+        // when
+        ChannelStatisticsDto response = youTubeClient.getStatisticsByChannel(channelId)
+                .block();
+
+        // then
+        assert response != null;
+        assertThatThrownBy(response::getStatistics)
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
